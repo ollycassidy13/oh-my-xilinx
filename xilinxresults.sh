924ab2210e6d5cb0bcc80187255d652d1936f92f
Nachiket Kapre <nachiket@ieee.org>
2014-12-26 14:02:40 +0800

    committing stuff

--- xilinxresults.sh
+++ xilinxresults.sh
@@ -6,13 +6,16 @@ echo ${2:=0} > /dev/null
 area=`cat results_$1/$1\_usage.xml | grep -A 1 "AGG_SLICE" | sed 's/.*value="\(.*\)".*/\1/' | tail -n 1`
 dsp=`cat results_$1/$1\_usage.xml | grep -A 1 "DSP" | sed 's/.*value="\(.*\)".*/\1/' | tail -n 1`
 bram=`cat results_$1/$1\_usage.xml | grep -A 1 "RAMB" | sed 's/.*value="\(.*\)".*/\1/' | tail -n 1`
-clk=`cat results_$1/$1.twr | grep "CP achieved:" | sed "s/CP achieved:\ *//g"`
+#I think this is for HLS/Vivado clock period
+#clk=`cat results_$1/$1.twr | grep "CP achieved:" | sed "s/CP achieved:\ *//g"`
+#I think this is for ISE clock period
+clk=`cat results_$1/$1.twr | grep "Minimum period:" | sed "s/.*:\(.*\){.*/\1/"`
 
 echo ${area:=0} ${dsp:=0} ${bram:=0} ${clk:=0} > /dev/null
 
 if [[ $2 -eq 0 ]]; then
 	printf "bench area dsp bram clk\n" $1 $area $dsp $bram $clk
-	printf "%35s %5d %5d %5d %5d\n" $1 $area $dsp $bram $clk
+	printf "%35s %5d %5d %5d %10s\n" $1 $area $dsp $bram $clk
 else
 	echo \'$area\', \'$dsp\', \'$bram\', \'$clk\'
 fi
