e9ba3affbf5ca7789f23d13d56ece908d137f254
Nachiket Kapre <nachiket@ieee.org>
2015-11-19 15:58:14 +0800

    ok this is annoying

--- xilinxcompile.sh
+++ xilinxcompile.sh
@@ -30,14 +30,14 @@ for i in ../*.v
 do
 	echo "verilog work $i" >> ../$1.prj
 done
-for i in ../*.vhd
-do
-	echo "vhdl work $i" >> ../$1.prj
-done
-for i in ../*.vhdl
-do
-	echo "vhdl work $i" >> ../$1.prj
-done
+#for i in ../*.vhd
+#do
+#	echo "vhdl work $i" >> ../$1.prj
+#done
+#for i in ../*.vhdl
+#do
+#	echo "vhdl work $i" >> ../$1.prj
+#done
 
 # eval synth script (really stupid and needles...)
 cp -f ~/.oh-my-xilinx/xilinxcompile.scr ../$1.scr
@@ -50,6 +50,7 @@ cp ../*.ngc .
 
 # compile and map through the steps of xilinx flow..
 xst -ifn ../$1.scr 
+touch ../$1.ucf
 ngdbuild -verbose $1.ngc -uc ../$1.ucf
 map -w -u -timing -ol high -pr b -o mapped.ncd $1.ngd mapped.pcf
 par -ol std -w mapped.ncd $1.ncd mapped.pcf
--- xilinxresults.sh
+++ xilinxresults.sh
@@ -4,6 +4,7 @@
 echo ${2:=0} > /dev/null
 
 area=`cat results_$1/$1\_usage.xml | grep -A 1 "AGG_SLICE" | sed 's/.*value="\(.*\)".*/\1/' | tail -n 1`
+luts=`cat results_$1/$1\_usage.xml | grep -A 1 "NUM_BSUSED" | sed 's/.*value="\(.*\)".*/\1/' | tail -n 1`
 dsp=`cat results_$1/$1\_usage.xml | grep -A 1 "DSP" | sed 's/.*value="\(.*\)".*/\1/' | tail -n 1`
 bram=`cat results_$1/$1\_usage.xml | grep -A 1 "RAMB" | sed 's/.*value="\(.*\)".*/\1/' | tail -n 1`
 #I think this is for HLS/Vivado clock period
@@ -14,8 +15,8 @@ clk=`cat results_$1/$1.twr | grep "Minimum period:" | sed "s/.*:\(.*\){.*/\1/"`
 echo ${area:=0} ${dsp:=0} ${bram:=0} ${clk:=0} > /dev/null
 
 if [[ $2 -eq 0 ]]; then
-	printf "bench area dsp bram clk\n" $1 $area $dsp $bram $clk
-	printf "%35s %5d %5d %5d %10s\n" $1 $area $dsp $bram $clk
+	printf "bench,slices,luts,dsp,bram,clk\n" $1 $area $luts $dsp $bram $clk
+	printf "%s,%d,%d,%d,%d,%s\n" $1 $area $luts $dsp $bram $clk
 else
 	echo \'$area\', \'$dsp\', \'$bram\', \'$clk\'
 fi
