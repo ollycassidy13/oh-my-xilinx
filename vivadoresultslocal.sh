ac1391bad9b7a45d523cf2bda1f3a7a8b9c6a2fd
Nachiket Kapre <nachiket@uwaterloo.ca>
2016-12-28 10:39:49 -0500

    ahem

--- vivadoresultslocal.sh
+++ vivadoresultslocal.sh
@@ -6,7 +6,7 @@
 #	Board:		VC707 - Virtex-7 VX485T FPGA
 
 cat vivado.log | grep LUTs | grep -v SLR
-cat vivado.log | grep "Slice Registers"
+cat vivado.log | grep "Registers" | grep -v SLR | grep "^|" | grep -v LAGUNA
 cat vivado.log | grep DSPs | grep -v SLR 
 cat vivado.log | grep "Slack (MET)"
 cat vivado.log | grep "Slack (VIOLATED)"
